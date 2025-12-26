af2e45819857ccb5ce5401ffbbe77a829f0fa9ae
Sourav Dokania 🐟 <sudoku@amazon.com>
2025-12-26 10:44:49 -0800

    Fix asset paths for sub-routes (base: '/' instead of './') and remove _redirects

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
   return {
-    base: process.env.GITHUB_ACTIONS ? '/yun-wu-portfolio/' : './',
+    base: process.env.GITHUB_ACTIONS ? '/yun-wu-portfolio/' : '/',
     server: {
       port: 3000,
       host: '0.0.0.0',
